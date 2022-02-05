07127311f0824ea76b16ed1b9aa5bfbf82ef1576
sivakumar <sivakumarsc.me@gmail.com>
2022-02-05 21:36:59 +0530

    update star rating component

--- star/main.js
+++ star/main.js
@@ -1,14 +1,55 @@
 window.addEventListener("load", function() {
-  document.getElementById('star-wrapper').addEventListener("click", function(e) {
-    const targetId = e.target.id;
-    let stars = document.getElementsByClassName('star');
-    for (let i=0; i<stars.length; i+=1) {
-      if (i <= targetId) {
-        stars[i].classList.add('rated');
-      } else {
-        stars[i].classList.remove('rated');
+	const stars = document.getElementsByTagName("li");
+  const starContainer = document.getElementsByClassName("rating-container")[0];
+  let active = -1;
+  
+  starContainer.addEventListener("click", function(el) {
+  	if (el.target.dataset.id) {
+    	let currentIndex = el.target.dataset.id;
+      active = currentIndex - 1;
+      
+      for(let i=0; i<stars.length; i+=1) {
+      	if (i <= currentIndex - 1) {
+        	stars[i].classList.add("active");
+        } else {
+        	stars[i].classList.remove("active");
+        }
       }
     }
-    console.log(targetId);
+  });
+  starContainer.addEventListener("click", function(el) {
+  	if (el.target.dataset.id) {
+    	let currentIndex = el.target.dataset.id;
+      
+      for(let i=0; i<stars.length; i+=1) {
+      	if (i <= currentIndex - 1) {
+        	stars[i].classList.add("active");
+        } else {
+        	stars[i].classList.remove("active");
+        }
+      }
+    }
+  });
+  starContainer.addEventListener("mouseover", function(el) {
+  	if (el.target.dataset.id) {
+    	let currentIndex = el.target.dataset.id;
+      for(let i=0; i<stars.length; i+=1) {
+      	if (i <= currentIndex - 1) {
+        	stars[i].classList.add("active");
+        } else {
+        	stars[i].classList.remove("active");
+        }
+      }
+    }
+  });
+  starContainer.addEventListener("mouseleave", function(el) {
+      
+      for(let i=0; i<stars.length; i+=1) {
+      	if (i <= active) {
+        	stars[i].classList.add("active");
+        } else {
+        	stars[i].classList.remove("active");
+        }
+      }
   });
 });
